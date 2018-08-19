1ff030d82843957e91f28e3c97636057085773c5
Simon Sinding <sinding2000@gmail.com>
2018-08-19 12:12:13 +0200

    changed to more dynamic file

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -1,7 +1,9 @@
 const Firestore = require('@google-cloud/firestore');
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS === null) {
+  throw new Error('Google credentials is missing from env!');
+}
 const store = new Firestore({
   projectId: 'tea-project-211819',
-  keyFilename:
-    '/home/thesinding/code/fun/tea_backend/tea-project-211819-4771b9cc4d26.json'
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
 });
 module.exports = store;
