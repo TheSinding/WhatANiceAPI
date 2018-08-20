71446684337486b9532c4b18193a576b2d646ece
Simon Sinding <sinding2000@gmail.com>
2018-08-20 13:18:03 +0200

    sentence counter event

---
+++ src/hooks/sentence-counter.js
@@ -0,0 +1,35 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+const errors = require('@feathersjs/errors');
+// eslint-disable-next-line no-unused-vars
+module.exports = function(options = {}) {
+  return async context => {
+    const { method, service, app } = context;
+    const aggregationService = app.service('aggregations');
+    const SENTENCES = 'sentences';
+    try {
+      const sentenceAggregation = await aggregationService.get(SENTENCES);
+      if (!sentenceAggregation.exists) throw new errors['500']();
+
+      let currentCount = sentenceAggregation.data.count;
+
+      switch (method) {
+      case 'create':
+        currentCount++;
+        break;
+      case 'delete':
+        currentCount--;
+        break;
+      }
+      await aggregationService.patch(SENTENCES, { count: currentCount });
+
+      service.emit('counterChanged', {
+        type: 'counterChanged',
+        data: { currentCount }
+      });
+    } catch (error) {
+      throw error;
+    }
+    return context;
+  };
+};
