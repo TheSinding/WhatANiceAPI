c1d8e782ae6e2b9e2430456cc578d70eb258f2b0
Simon Sinding <sinding2000@gmail.com>
2018-08-20 13:19:16 +0200

    added files

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,13 @@
 const analyze = require('./analyze/analyze.service.js');
 const sentences = require('./sentences/sentences.service.js');
+const users = require('./users/users.service.js');
+const aggregations = require('./aggregations/aggregations.service.js');
+const counters = require('./counters/counters.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(analyze);
   app.configure(sentences);
+  app.configure(users);
+  app.configure(aggregations);
+  app.configure(counters);
 };
