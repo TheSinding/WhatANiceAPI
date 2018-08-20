591c1691ef5142366c4058120f21d2e1f2e6ed58
Simon Sinding <sinding2000@gmail.com>
2018-08-20 13:18:23 +0200

    made more generic

--- src/database/firestore.js
+++ src/database/firestore.js
@@ -1,9 +1,13 @@
 const Firestore = require('@google-cloud/firestore');
-if (process.env.GOOGLE_APPLICATION_CREDENTIALS === null) {
+if (
+  process.env.GOOGLE_APPLICATION_CREDENTIALS === null ||
+  process.env.GOOGLE_APPLICATION_CREDENTIALS === undefined
+) {
   throw new Error('Google credentials is missing from env!');
 }
 const store = new Firestore({
-  projectId: 'tea-project-211819',
-  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+  projectId: process.env.GOOGLE_APPLICATION_CREDENTIALS.project_id,
+  keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  timestampsInSnapshots: true
 });
 module.exports = store;
