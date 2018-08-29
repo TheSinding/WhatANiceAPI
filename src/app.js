ca96128a020f3dd6e89eea7d5755271f2b7c1ce3
Simon Sinding <sinding2000@gmail.com>
2018-08-29 21:28:58 +0200

    added mongodb

--- src/app.js
+++ src/app.js
@@ -17,6 +17,8 @@ const channels = require('./channels');
 
 const authentication = require('./authentication');
 
+const mongodb = require('./mongodb');
+
 const app = express(feathers());
 
 // Load app configuration
@@ -34,6 +36,8 @@ app.use('/', express.static(app.get('public')));
 // Set up Plugins and providers
 app.configure(express.rest());
 app.configure(socketio());
+
+app.configure(mongodb);
 
 // Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
