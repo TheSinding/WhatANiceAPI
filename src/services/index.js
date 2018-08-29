b07264d720f11569999595102929c06c55ae85b9
Simon Sinding <sinding2000@gmail.com>
2018-08-29 21:31:38 +0200

    removed services

--- src/services/index.js
+++ src/services/index.js
@@ -1,15 +1,9 @@
 const analyze = require('./analyze/analyze.service.js');
 const sentences = require('./sentences/sentences.service.js');
 const users = require('./users/users.service.js');
-const aggregations = require('./aggregations/aggregations.service.js');
-const counters = require('./counters/counters.service.js');
-const catalogue = require('./catalogue/catalogue.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(analyze);
   app.configure(sentences);
   app.configure(users);
-  app.configure(aggregations);
-  app.configure(counters);
-  app.configure(catalogue);
 };
