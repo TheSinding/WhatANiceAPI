bf72626b53917416f1b55405bcd9df3b63722a35
Simon Sinding <sinding2000@gmail.com>
2018-08-30 17:27:46 +0200

    apikeys and aggregations

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,13 @@
 const analyze = require('./analyze/analyze.service.js');
 const sentences = require('./sentences/sentences.service.js');
 const users = require('./users/users.service.js');
+const aggregations = require('./aggregations/aggregations.service.js');
+const apikeys = require('./apikeys/apikeys.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(analyze);
   app.configure(sentences);
   app.configure(users);
+  app.configure(aggregations);
+  app.configure(apikeys);
 };
