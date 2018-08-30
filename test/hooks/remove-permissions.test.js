7d66208b7957abe08caab42bf4a25e11521165a6
Simon Sinding <sinding2000@gmail.com>
2018-08-31 00:08:22 +0200

    tests

---
+++ test/hooks/remove-permissions.test.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const feathers = require('@feathersjs/feathers');
+const removePermissions = require('../../src/hooks/remove-permissions');
+
+describe('\'removePermissions\' hook', () => {
+  let app;
+
+  beforeEach(() => {
+    app = feathers();
+
+    app.use('/dummy', {
+      async get(id) {
+        return { id };
+      }
+    });
+
+    app.service('dummy').hooks({
+      before: removePermissions()
+    });
+  });
+
+  it('runs the hook', async () => {
+    const result = await app.service('dummy').get('test');
+    
+    assert.deepEqual(result, { id: 'test' });
+  });
+});
