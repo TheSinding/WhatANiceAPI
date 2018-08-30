6429fd0fbd8256f1b0e10a47d90678ab33d8044d
Simon Sinding <sinding2000@gmail.com>
2018-08-30 17:27:35 +0200

    changed from mongodb to mongoose

--- src/services/sentences/sentences.service.js
+++ src/services/sentences/sentences.service.js
@@ -1,21 +1,18 @@
 // Initializes the `sentences` service on path `/sentences`
-const createService = require('feathers-mongodb');
+const createService = require('feathers-mongoose');
 const hooks = require('./sentences.hooks');
+const createModel = require('../../models/sentences.model');
 
 module.exports = function(app) {
   const paginate = app.get('paginate');
-  const mongoClient = app.get('mongoClient');
-  const options = { paginate };
+  const Model = createModel(app);
+  const options = { Model, paginate };
 
   // Initialize our service with any options it requires
   app.use('/sentences', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('sentences');
-
-  mongoClient.then(db => {
-    service.Model = db.collection('sentences');
-  });
 
   service.hooks(hooks);
 };
