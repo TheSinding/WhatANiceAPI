b1235e273572b848d176042da583b3b21a810189
Simon Sinding <sinding2000@gmail.com>
2018-08-31 00:04:48 +0200

    checks users permissions

---
+++ src/hooks/check-user-permissions.js
@@ -0,0 +1,26 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+
+// eslint-disable-next-line no-unused-vars
+const errors = require('@feathersjs/errors');
+module.exports = function(options = {}) {
+  return async context => {
+    const { params, path, method, app } = context;
+    const protectedRoutes = app.get('protectedRoutes');
+    // If other serives uses the users method eg, authentication, params.user will be null
+    const { user = null } = params;
+    if (user === null) return context;
+
+    const { permissions } = user;
+
+    if (!(path in protectedRoutes)) return context;
+
+    if (!(method in protectedRoutes[path])) return context;
+
+    for (const permission of permissions) {
+      if (protectedRoutes[path][method].includes(permission)) return context;
+    }
+
+    throw new errors.MethodNotAllowed('You have insuffient permissions.');
+  };
+};
