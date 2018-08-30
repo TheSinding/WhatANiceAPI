4d141596f49bac686c44215e3a62c056bbd3403e
Simon Sinding <sinding2000@gmail.com>
2018-08-31 00:08:05 +0200

    only get sentece with apikey

--- src/services/sentences/sentences.hooks.js
+++ src/services/sentences/sentences.hooks.js
@@ -1,10 +1,12 @@
 const sentencesViewedCounter = require('../../hooks/sentences-viewed-counter');
 const getRandomSentence = require('../../hooks/get-random-sentence');
 const sentenceCounter = require('../../hooks/sentence-counter');
+const checkApikeyScope = require('../../hooks/check-apikey-scope');
+const findApikey = require('../../hooks/find-apikey');
 
 module.exports = {
   before: {
-    all: [],
+    all: [findApikey(), checkApikeyScope()],
     find: [getRandomSentence()],
     get: [],
     create: [],
