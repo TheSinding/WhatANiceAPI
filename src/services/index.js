053f8f45ef6aca62ab5af3d0726eb2bea585560a
Simon Sinding <sinding2000@gmail.com>
2018-09-18 15:32:16 +0200

    Add metrics, to get all metrics from one place

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const analyze = require('./analyze/analyze.service.js');
 const sentences = require('./sentences/sentences.service.js');
 const users = require('./users/users.service.js');
 const aggregations = require('./aggregations/aggregations.service.js');
+const metrics = require('./metrics/metrics.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(analyze);
   app.configure(sentences);
   app.configure(users);
   app.configure(aggregations);
+  app.configure(metrics);
 };
