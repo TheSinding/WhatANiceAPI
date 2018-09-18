ccda5f33c9561032fc2703f975f2fe56ae465ca3
Simon Sinding <sinding2000@gmail.com>
2018-09-18 15:34:54 +0200

    Adds channels for sentences to publish changes to

--- src/services/sentences/sentences.service.js
+++ src/services/sentences/sentences.service.js
@@ -6,13 +6,20 @@ const createModel = require('../../models/sentences.model');
 module.exports = function(app) {
   const paginate = app.get('paginate');
   const Model = createModel(app);
-  const options = { Model, paginate };
+  const options = { Model, paginate, events: ['used', 'daysCount'] };
 
   // Initialize our service with any options it requires
   app.use('/sentences', createService(options));
 
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('sentences');
+
+  service.publish('used', () => {
+    return app.channel('everybody');
+  });
+  service.publish('daysCount', () => {
+    return app.channel('everybody');
+  });
 
   service.hooks(hooks);
 };
