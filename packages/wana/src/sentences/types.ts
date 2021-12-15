6454d8331d25b3c0263e4acf358034d7464e9b14
Simon Sinding <simon.sinding@danfoss.com>
2021-12-15 09:22:25 +0100

    feat: adds tags to sentences

--- packages/wana/src/sentences/types.ts
+++ packages/wana/src/sentences/types.ts
@@ -1,17 +1,36 @@
 import { Static, Type } from "@sinclair/typebox";
 
-export const SentenceSchema = Type.Object(
-  {
-    id: Type.String(),
-    content: Type.String(),
-    timestamp: Type.Integer(),
-  },
-  { additionalProperties: false }
+export const SentenceSchema = Type.Pick(
+  Type.Object(
+    {
+      id: Type.String(),
+      content: Type.String({
+        description: "The actual sentence",
+        examples: "You look awesome today!",
+      }),
+      createdAt: Type.Integer({
+        description: "The timestamp of creation",
+      }),
+      updatedAt: Type.Integer({
+        description: "The timestamp of an update",
+      }),
+      tags: Type.Array(Type.String(), {
+        description: "Array of tags, describing the sentence",
+        examples: ["love", "nice", "business", "greeting"],
+      }),
+    },
+    { additionalProperties: false }
+  ),
+  ["id", "content", "createdAt", "tags"]
 );
 
-export const SentenceBodySchema = Type.Object({
-  content: Type.String(),
-});
+export const SentenceBodySchema = Type.Pick(
+  Type.Object({
+    content: SentenceSchema.properties.content,
+    tags: SentenceSchema.properties.tags,
+  }),
+  ["content"]
+);
 
 export type Sentence = Static<typeof SentenceSchema>;
 export type SenteceBody = Static<typeof SentenceBodySchema>;
